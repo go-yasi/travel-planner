ad67f30fd6caeb519288558a45f2ed489df70b51
lillie <lillie@Lillies-MacBook-Pro.local>
2021-06-22 12:03:05 -0700

    added route on tripRoutes.js

--- routes/api/tripRoutes.js
+++ routes/api/tripRoutes.js
@@ -1,3 +1,10 @@
+const router = require('express').Router();
+const {Trip, Traveller, Location } = require('../../models');
+
+
+
 // POST route /api/trips creates trip data between associated travellers and locations.
+
+
 
 // DELETE route /api/trips/:id removes a trip 
