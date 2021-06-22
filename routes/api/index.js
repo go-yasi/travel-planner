3c1ac99b1ec07dc123dbb48e3e36146ec8084f2a
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-06-22 11:51:37 -0700

    Added individual api routes to index.js

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const locationRoutes = require('./locationRoutes');
+const travellerRoutes = require('./travellerRoutes');
+const tripRoutes = require('./tripRoutes');
+
+router.use('/location', locationRoutes);
+router.use('/traveller', travellerRoutes);
+router.use('/trip', tripRoutes);
+
+module.exports = router;
