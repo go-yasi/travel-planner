df94166c808f88a53a381a0ddda77c0458176de9
lillie <lillie@Lillies-MacBook-Pro.local>
2021-06-22 13:53:11 -0700

    updated tripRoutes.js

--- routes/api/tripRoutes.js
+++ routes/api/tripRoutes.js
@@ -6,9 +6,7 @@ const { Trip, Traveller, Location } = require('../../models');
 // POST route /api/trips creates trip data between associated travellers and locations.
 router.post('/', async (req, res) => {
     try {
-        const tripData = await Trip.create({
-            include: [{ model: Traveller }, { model: Location }],
-        });
+        const tripData = await Trip.create(req.body);
         res.status(200).json(tripData);
     } catch (err) {
         res.status(400).json(err);
