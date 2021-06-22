b86b7bd7d904a0052b37d44a39207e8e60583c3f
lillie <lillie@Lillies-MacBook-Pro.local>
2021-06-22 12:14:24 -0700

    added Post route on tripRouters.js

--- routes/api/tripRoutes.js
+++ routes/api/tripRoutes.js
@@ -1,10 +1,23 @@
 const router = require('express').Router();
-const {Trip, Traveller, Location } = require('../../models');
+const { Trip, Traveller, Location } = require('../../models');
 
 
 
 // POST route /api/trips creates trip data between associated travellers and locations.
-
-
+router.post('/', async (req, res) => {
+    try {
+        const tripData = await Trip.create({
+            include: [{ model: Traveller }, { model: Location }],
+        });
+    res.status(200).json(tripData);
+} catch (err) {
+res.status(400).json(err);
+}
+})
 
 // DELETE route /api/trips/:id removes a trip 
+router.delete('/:id', async (req,res) => {
+    try {
+        const tripData = await Trip.destroy
+    }
+})
